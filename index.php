44699f5b91ce3c3af306b09cdc2be252264bd32d
root <sackboy11@gmail.com>
2019-05-17 20:23:13 +0000

    table setup, needs styling

--- index.php
+++ index.php
@@ -16,17 +16,17 @@
 <body>
     <div class="file-json" style='display: none;'>
         <?php
-                if(isauth){
-                    //todo: recursive file search
-                }
-            ?>
+            if($isauth){
+                //todo: recursive file search
+            }
+        ?>
     </div>
-    <div class="login" <?php if(!isauth) { echo "style='display: none;'"; } ?>>
+    <div class="login" <?php if($isauth) { echo "style='display: none;'"; } ?>>
         <div class="portal">
             <center>
                 File Server
                 <h1>Login</h1>
-                <form mehtod="post">
+                <form method="post">
                     <input type="text" class="text-in" name="passphrase" type="password">
                     <br>
                     <input type="submit" class="btn" value="login">
@@ -35,8 +35,28 @@
 
         </div>
     </div>
-    <div class="files" <?php if(isauth) { echo "style='display: none;'"; } ?>>
-        woop
+    <div class="files" <?php if(!$isauth) { echo "style='display: none;'"; } ?>>
+        <div class="cur-path">
+        </div>
+        <table>
+            <table>
+                <tbody class="files">
+                    <tr id="fldr">
+                        <td>
+                            <center><i class="icon folder"></i></center>
+                        </td>
+                        <td>Example Name</td>
+                    </tr>
+                    <tr id="fl">
+                        <td>
+                            <center><i class="icon file"></i></center>
+                        </td>
+                        <td class="file-name">Example Name</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </table>
     </div>
 </body>
 
