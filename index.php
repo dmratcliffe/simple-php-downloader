f0ef18fb44b5ac5dcb2ddfe23615db2f47622959
root <sackboy11@gmail.com>
2019-05-17 19:55:16 +0000

    login portal style is done

--- index.php
+++ index.php
@@ -3,15 +3,41 @@
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>
-            File Downloader
-        </title>
-        <link href="assets/css/styles.css" rel="stylesheet" />
-        <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-        <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-    </head>
-    <body>
 
-    </body>
+<head>
+    <title>
+        File Downloader
+    </title>
+    <link href="assets/css/styles.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</head>
+
+<body>
+    <div class="file-json" style='display: none;'>
+        <?php
+                if(isauth){
+                    //todo: recursive file search
+                }
+            ?>
+    </div>
+    <div class="login" <?php if(!isauth) { echo "style='display: none;'"; } ?>>
+        <div class="portal">
+            <center>
+                File Server
+                <h1>Login</h1>
+                <form mehtod="post">
+                    <input type="text" class="text-in" name="passphrase">
+                    <br>
+                    <input type="submit" class="btn" value="login">
+                </form>
+            </center>
+
+        </div>
+    </div>
+    <div class="files" <?php if(isauth) { echo "style='display: none;'"; } ?>>
+        woop
+    </div>
+</body>
+
 </html>
