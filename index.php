ebb1320dffc25751ba19aa9991facc7e42addf67
root <sackboy11@gmail.com>
2019-05-20 22:35:56 +0000

    Style is finished.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         File Downloader
     </title>
     <link href="assets/css/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="assets/js/script.js"></script>
 </head>
@@ -37,21 +38,22 @@
     </div>
     <div class="files" <?php if(!$isauth) { echo "style='display: none;'"; } ?>>
         <div class="cur-path">
+        /this/is/your/current/path
         </div>
         <table>
             <table>
                 <tbody class="files">
                     <tr id="fldr">
                         <td>
-                            <center><i class="icon folder"></i></center>
+                            <center><i class="icon fas fa-folder"></i></center>
                         </td>
                         <td>Example Name</td>
                     </tr>
                     <tr id="fl">
                         <td>
-                            <center><i class="icon file"></i></center>
+                            <center><i class="icon fas fa-file-alt"></i></center>
                         </td>
-                        <td class="file-name">Example Name</td>
+                        <td class="item-name">Example Name</td>
                     </tr>
 
                 </tbody>
